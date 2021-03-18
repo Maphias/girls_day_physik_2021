035dc71d0eef47eed492a28fd7665709f1ee9e27
Maphias <55281567+Maphias@users.noreply.github.com>
2021-03-18 17:42:23 +0000

    Deploying to gh-pages from master @ 71ceb43846067709cb7e45173330ee31fede3f48 🚀

--- Labratory.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="Labratory" tilewidth="32" tileheight="32" tilecount="324" columns="27">
- <image source="tilesets/Labratory.png" width="876" height="385"/>
-</tileset>
--- future-joy-tilee_750.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.4" tiledversion="1.4.3" name="future-joy-tilee_750" tilewidth="32" tileheight="32" tilecount="256" columns="16">
- <image source="tilesets/future-joy-tilee_750.png" width="512" height="512"/>
-</tileset>
